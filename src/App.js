8f430e5d6566ead14661b04b69b78303ade0f26a
Rajeev Krishna <lambo2572@gmail.com>
2023-07-01 16:23:41 +0530

    Error issues revamping project

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
-import { Routes, Route } from "react-router-dom";
-import { Navbar } from "./components/navbar";
-import { Home } from "./pages/Home";
-import { Product } from "./pages/Product";
-import { Cart } from "./pages/Cart";
-import { Footer } from "./components/footer";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/product" element={<Product />} />
-        <Route path="/cart" element={<Cart />} />
-      </Routes>
-      <Footer />
+
     </div>
   );
 }
--- src/components/footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Footer = () => {
-  return (
-    <div className='py-10 text-center'>
-        <h1 className=''>This is the footer section</h1>
-    </div>
-  )
-}
